b3192392c3a61ff264682268622aa50766fe08ff
Ansel <aesthapus@gmail.com>
2015-03-02 01:02:23 -0500

    fleshed out remaster-image.sh, but it's still more of a copy-paste thing...

--- remaster-image.sh
+++ remaster-image.sh
@@ -1,40 +1,300 @@
 #! /usr/bin/bash
 
+if [ `lsb_release -is` == "Debian" ]; then
+
+	echo "Debian mode..."
+
 # Get started and extract the iso
-mkdir mnt extract
-mount -o loop *iso mnt/
-rsync --exclude=/live/filesystem.squashfs -a mnt/ extract
-unsquashfs mnt/live/filesystem.squashfs
-mv squashfs-root edit
-umount mnt/
+	mkdir mnt extract
+	sudo mount -o loop *iso mnt/
+	sudo rsync --exclude=/live/filesystem.squashfs -a mnt/ extract
+	sudo unsquashfs mnt/live/filesystem.squashfs
+	mv squashfs-root edit
+	sudo umount mnt/
 
 # Prepare to chroot into the extracted iso
-cp /etc/resolv.conf edit/etc/
+	sudo cp /etc/resolv.conf edit/etc/
+# copy RT kernel *.deb files and the Xenomai build
 
-# chroot
-mount --bind /dev/ edit/dev
-chroot edit
+# Enter the chroot
+	sudo mount --bind /dev/ edit/dev
+	sudo chroot edit
 
-mount -t proc none /proc
-mount -t sysfs none /sys
-mount -t devpts none /dev/pts
-export HOME=/root
-export LC_ALL=C
+# Mount things and prepare environment
+	mount -t proc none /proc
+	mount -t sysfs none /sys
+	mount -t devpts none /dev/pts
+	export HOME=/root
+	export LC_ALL=C
 
-# do extra stuff here
+# Make things take on dark theme (cinnamon, maybe gnome)
+	touch /etc/gtk-3.0/settings.ini
+	echo "[Settings]" >> /etc/gtk-3.0/settings.ini
+	echo "gtk-application-prefer-dark-theme=1" >> /etc/gtk-3.0/settings.ini
 
-umount /proc /sys /dev/pts
-exit
+# Install RT kernel and RTXI
+	cd ~/
+	mkdir .config
+	gdebi linux-image*
+	gdebi linux-headers*
+	git clone https://github.com/rtxi/rtxi
+	cd rtxi
+	git checkout qt4
+	cd scripts
+	
+	DIR=$PWD
+	ROOT=${DIR}/../
+	DEPS=${ROOT}/deps
+	HDF=${DEPS}/hdf
+	QWT=${DEPS}/qwt
+	DYN=${DEPS}/dynamo
+	cd ${DEPS}
 
-umount edit/dev
-sudo bash -c "chroot edit dpkg-query -W  > extract/live/filesystem.packages"
-cp edit/boot/vmlinuz-3.16.0-4-amd64 extract/live/vmlinuz
-cp edit/boot/initrd.img-3.16.0-4-amd64 extract/live/initrd.img
-mksquashfs edit extract/live/filesystem.squashfs -comp xz
+# Installing HDF5
+	echo "----->Checking for HDF5"
 
-# don't know if this is necessary
-sudo bash -c "find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt"
+	if [ -f "/usr/include/hdf5.h" ]; then
+		echo "----->HDF5 already installed."
+	else
+		echo "----->Installing HDF5..."
+		cd ${HDF}
+		tar xf hdf5-1.8.4.tar.bz2
+		cd hdf5-1.8.4
+		./configure --prefix=/usr
+		make -sj2
+		sudo make install
+		if [ $? -eq 0 ]; then
+				echo "----->HDF5 installed."
+		else
+			echo "----->HDF5 installation failed."
+			exit
+		fi
+	fi
 
-#xorriso -as mkisofs -r -J -joliet-long -l -cache-inodes -isohybrid-mbr /usr/lib/syslinux/isohdpfx.bin -partition_offset 16 -A "RTXI Live"  -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o rtxi-2.0-debian-8.0-v1-x86_64.iso binary
+# Installing Qwt
+	echo "----->Checking for Qwt"
 
-genisoimage -D -r -V "RTXI" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../custom.iso . 
+	if [ -f "/usr/local/lib/qwt/include/qwt.h" ]; then
+		echo "----->Qwt already installed."
+	else
+		echo "----->Installing Qwt..."
+		cd ${QWT}
+		tar xf qwt-6.1.0.tar.bz2
+		cd qwt-6.1.0
+		qmake qwt.pro
+		make -sj2
+		sudo make install
+		sudo cp /usr/local/lib/qwt/lib/libqwt.so.6.1.0 /usr/lib/.
+		sudo ln -sf /usr/lib/libqwt.so.6.1.0 /usr/lib/libqwt.so
+		sudo ldconfig
+		if [ $? -eq 0 ]; then
+			echo "----->Qwt installed."
+		else
+			echo "----->Qwt installation failed."
+		exit
+		fi
+	fi
+
+# Install rtxi_includes
+	sudo rsync -a ${DEPS}/rtxi_includes /usr/local/lib/.
+	if [ $? -eq 0 ]; then
+		echo "----->rtxi_includes synced."
+	else
+		echo "----->rtxi_includes sync failed."
+		exit
+	fi
+	find ../plugins/. -name "*.h" -exec cp -t /usr/local/lib/rtxi_includes/ {} +
+
+# Install dynamo
+	echo "Installing DYNAMO utility..."
+
+	cd ${DYN}
+	mllex dl.lex
+	mlyacc dl.grm
+	mlton dynamo.mlb
+	sudo cp dynamo /usr/bin/
+	if [ $? -eq 0 ]; then
+		echo "----->DYNAMO translation utility installed."
+	else
+		echo "----->DYNAMO translation utility installation failed."
+		exit
+	fi
+	
+# Install RTXI
+	./install_rtxi.sh # needs user to enter 1 in prompt
+
+# Install ggplot
+	R
+	install.packages("ggplot2") # use mirror 96 if you want
+	q() # "n" - don't save workspace
+
+# Clean environment and exit
+	rm /etc/resolv.conf
+	umount /proc /sys /dev/pts
+	exit
+	sudo umount edit/dev
+
+# Update files in live/ directory
+	sudo bash -c "chroot edit dpkg-query -W  > extract/live/filesystem.packages"
+	sudo cp edit/boot/vmlinuz-3.8.13-xenomai-2.6.3-aufs extract/live/vmlinuz
+	sudo cp edit/boot/initrd.img-3.8.13-xenomai-2.6.3-aufs extract/live/initrd.img
+	sudo mksquashfs edit extract/live/filesystem.squashfs -comp xz
+
+	cd extract
+	sudo bash -c "find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt"
+
+	sudo genisoimage -D -r -V "RTXI" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../custom.iso . 
+
+	echo "Done...I think."
+
+elif [ `lsb_release -is` == "Ubuntu" ]; then
+
+	echo "Ubuntu mode...ISN'T DONE SO DON'T USE IT!!!!!"
+	exit
+
+# Get started and extract the iso
+	mkdir mnt extract
+	sudo mount -o loop *iso mnt/
+	sudo rsync --exclude=/casper/filesystem.squashfs -a mnt/ extract
+	sudo unsquashfs mnt/live/filesystem.squashfs
+	mv squashfs-root edit
+	sudo umount mnt/
+
+# Prepare to chroot into the extracted iso
+	sudo cp /etc/resolv.conf edit/run/resolvconf/resolv.conf
+# copy RT kernel *.deb files and the Xenomai build
+
+# Enter the chroot
+	sudo mount --bind /dev/ edit/dev
+	sudo chroot edit
+
+# Mount things and prepare environment
+	mount -t proc none /proc
+	mount -t sysfs none /sys
+	mount -t devpts none /dev/pts
+	export HOME=/root
+	export LC_ALL=C
+
+# Install dependencies
+	git clone https://github.com/rtxi/rtxi
+	cd rtxi
+	git checkout qt4
+	cd scripts
+	DIR=$PWD
+	ROOT=${DIR}/../
+	DEPS=${ROOT}/deps
+	HDF=${DEPS}/hdf
+	QWT=${DEPS}/qwt
+	DYN=${DEPS}/dynamo
+	sudo apt-get -y install autotools-dev automake libtool
+	sudo apt-get -y install kernel-package
+	sudo apt-get -y install fakeroot crash kexec-tools makedumpfile kernel-wedge # Select "No" for kexec handling restarts
+	sudo apt-get -y build-dep linux
+	sudo apt-get -y install git-core libncurses5 libncurses5-dev libelf-dev binutils-dev libgsl0-dev vim stress libboost-dev
+	sudo apt-get -y install qt4-dev-tools libqt4-dev libqt4-opengl-dev
+
+	cd ${DEPS}
+
+# Installing HDF5
+	echo "----->Checking for HDF5"
+
+	if [ -f "/usr/include/hdf5.h" ]; then
+		echo "----->HDF5 already installed."
+	else
+		echo "----->Installing HDF5..."
+		cd ${HDF}
+		tar xf hdf5-1.8.4.tar.bz2
+		cd hdf5-1.8.4
+		./configure --prefix=/usr
+		make -sj2
+		sudo make install
+		if [ $? -eq 0 ]; then
+				echo "----->HDF5 installed."
+		else
+			echo "----->HDF5 installation failed."
+			exit
+		fi
+	fi
+
+# Installing Qwt
+	echo "----->Checking for Qwt"
+
+	if [ -f "/usr/local/lib/qwt/include/qwt.h" ]; then
+		echo "----->Qwt already installed."
+	else
+		echo "----->Installing Qwt..."
+		cd ${QWT}
+		tar xf qwt-6.1.0.tar.bz2
+		cd qwt-6.1.0
+		qmake qwt.pro
+		make -sj2
+		sudo make install
+		sudo cp /usr/local/lib/qwt/lib/libqwt.so.6.1.0 /usr/lib/.
+		sudo ln -sf /usr/lib/libqwt.so.6.1.0 /usr/lib/libqwt.so
+		sudo ldconfig
+		if [ $? -eq 0 ]; then
+			echo "----->Qwt installed."
+		else
+			echo "----->Qwt installation failed."
+		exit
+		fi
+	fi
+
+# Install rtxi_includes
+	sudo rsync -a ${DEPS}/rtxi_includes /usr/local/lib/.
+	if [ $? -eq 0 ]; then
+		echo "----->rtxi_includes synced."
+	else
+		echo "----->rtxi_includes sync failed."
+		exit
+	fi
+	find ../plugins/. -name "*.h" -exec cp -t /usr/local/lib/rtxi_includes/ {} +
+
+# Install dynamo
+	echo "Installing DYNAMO utility..."
+
+	sudo apt-get -y install mlton
+	cd ${DYN}
+	mllex dl.lex
+	mlyacc dl.grm
+	mlton dynamo.mlb
+	sudo cp dynamo /usr/bin/
+	if [ $? -eq 0 ]; then
+		echo "----->DYNAMO translation utility installed."
+	else
+		echo "----->DYNAMO translation utility installation failed."
+		exit
+	fi
+
+# Install RT kernel and RTXI
+	cd ~/
+	mkdir .config
+	gdebi linux-image*
+	gdebi linux-headers*
+	./install_rtxi.sh # needs user to enter 1 in prompt
+
+# Install ggplot
+	R
+	install.packages("ggplot2") # use mirror 96 if you want
+	q() # "n" - don't save workspace
+
+# Clean environment and exit
+	rm /etc/resolv.conf
+	umount /proc /sys /dev/pts
+	exit
+	sudo umount edit/dev
+
+# Update files in live/ directory
+	sudo bash -c "chroot edit dpkg-query -W  > extract/live/filesystem.packages"
+	sudo cp edit/boot/vmlinuz-3.8.13-xenomai-2.6.3-aufs extract/live/vmlinuz
+	sudo cp edit/boot/initrd.img-3.8.13-xenomai-2.6.3-aufs extract/live/initrd.img
+	sudo mksquashfs edit extract/live/filesystem.squashfs -comp xz
+
+	cd extract
+	sudo bash -c "find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt"
+
+	sudo genisoimage -D -r -V "RTXI" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../custom.iso . 
+	echo "Done...maybe."
+else
+	echo "Sorry, I don't support other distros."
+fi
