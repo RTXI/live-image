3e147b0562fc9a23b16bc11cdc4c28dbaad94d08
Ansel <aesthapus@gmail.com>
2015-10-27 14:29:53 -0400

    Updated live-CD build script:
     * Added variable for Ubuntu versions
     * Updated comments
     * Changed line that copied casper filesystem kernels to use version
       variables

--- build-from-scratch.sh
+++ build-from-scratch.sh
@@ -13,9 +13,9 @@ sudo apt-get -y install genisoimage squashfs-tools syslinux
 # Global variables go here. 
 ###############################################################################
 
-# These two aren't used in the script. Oh well. 
 XENOMAI_VERSION=2.6.4
 KERNEL_VERSION=3.8.13
+UBUNTU_VERSION=14.04.3
 
 ROOT=$(pwd)
 BUILD=build_$(date +%F_%T)
@@ -35,9 +35,9 @@ mkdir ${BUILD}
 cd ${BUILD}
 
 if [ $ARCH == "amd64" ]; then
-	wget --no-check-certificate http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.2/release/ubuntu-gnome-14.04.2-desktop-amd64.iso
+	wget --no-check-certificate http://cdimage.ubuntu.com/ubuntu-gnome/releases/trusty/release/ubuntu-gnome-$UBUNTU_VERSION-desktop-amd64.iso
 else
-	wget --no-check-certificate http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.2/release/ubuntu-gnome-14.04.2-desktop-i386.iso
+	wget --no-check-certificate http://cdimage.ubuntu.com/ubuntu-gnome/releases/trusty/release/ubuntu-gnome-$UBUNTU_VERSION-desktop-i386.iso
 fi
 
 # Get started and extract the iso
@@ -55,8 +55,8 @@ sudo umount mnt/
 # Prepare to chroot into the extracted iso
 #sudo cp /etc/resolv.conf edit/run/resolvconf/resolv.conf <-For non-ubuntu systems
 sudo cp /run/resolvconf/resolv.conf edit/run/resolvconf/resolv.conf
-sudo cp /etc/apt/sources.list edit/etc/apt/sources.list # the live-cd sources.list is incomplete
-sudo cp $ROOT/deb_files/*.deb edit/root/
+sudo cp /etc/apt/sources.list edit/etc/apt/sources.list # the live-cd sources.list is incomplete, so replace it with your own
+sudo cp $ROOT/deb_files/*.deb edit/root/ # assumes pre-compiled rt kernel *.deb files are in deb_files/
 #sudo cp -r /usr/xenomai edit/usr/
 # ^- this can cause problems later...
 
@@ -64,7 +64,7 @@ sudo cp $ROOT/deb_files/*.deb edit/root/
 echo "Time to chroot this mother."
 echo "(This may take a while. Go outside. Frolic. Eat a sandwich.)"
 sudo mount --bind /dev/ edit/dev
-sudo cp $ROOT/chroot-script.sh edit/
+sudo cp $ROOT/chroot-script.sh edit/ # copy the chroot script into the chroot environment
 
 ###############################################################################
 # CHROOT! 
@@ -82,15 +82,17 @@ sudo rm edit/chroot-script.sh
 ###############################################################################
 # Update the image md5sum, package lists, etc. with changed introduced by the
 # chroot script, and then regenerate a compressed squashfs filesystem. 
+#
+# Also, overwrite the casper boot image with the RT-aufs versions. 
 ###############################################################################
 
 sudo bash -c "chroot edit dpkg-query -W > extract/casper/filesystem.manifest"
 if [ $ARCH == "amd64" ]; then
-	sudo cp edit/boot/vmlinuz-3.8.13-xenomai-2.6.3-aufs extract/casper/vmlinuz.efi
+	sudo cp edit/boot/vmlinuz-$KERNEL_VERSION-xenomai-$XENOMAI_VERSION-aufs extract/casper/vmlinuz.efi
 else
-	sudo cp edit/boot/vmlinuz-3.8.13-xenomai-2.6.3-aufs extract/casper/vmlinuz
+	sudo cp edit/boot/vmlinuz-$KERNEL_VERSION-xenomai-$XENOMAI_VERSION-aufs extract/casper/vmlinuz
 fi
-sudo cp edit/boot/initrd.img-3.8.13-xenomai-2.6.3-aufs extract/casper/initrd.lz
+sudo cp edit/boot/initrd.img-$KERNEL_VERSION-xenomai-$XENOMAI_VERSION-aufs extract/casper/initrd.lz
 sudo mksquashfs edit extract/casper/filesystem.squashfs -comp xz
 sudo bash -c "printf $(sudo du -sx --block-size=1 edit | cut -f1) > extract/casper/filesystem.size"
 
